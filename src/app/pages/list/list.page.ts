e08abf1096a306f5b593e59b1ad1e11fe1ee1376
Robin Delbaere <hello@rdelbaere.fr>
2021-04-12 16:53:51 +0200

    Correction : UI

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-list',
@@ -7,15 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
     private places: Array<any> = [
-        // {
-        //     id: 
-        // },
-
+        {
+            id: 1,
+            name: 'Raptor Shack',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim quidem autem dolore optio cupiditate dignissimos doloremque maiores repellat? Repudiandae quod accusamus consequuntur autem facere, nisi similique aperiam quia asperiores?',
+            location: {
+                latitude: 50.6321028,
+                longitude: 3.0619513,
+                address: '12 rue de rivoli - 59000'
+            }, 
+            image: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/5e/6d/eb/restaurant.jpg',
+            note: 4.3,
+            category: 'Burger',
+            createdAt: new Date(),
+        },
+        {
+            id: 2,
+            name: 'Il bastione',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim quidem autem dolore optio cupiditate dignissimos doloremque maiores repellat? Repudiandae quod accusamus consequuntur autem facere, nisi similique aperiam quia asperiores?',
+            location: {
+                latitude: 50.6321028,
+                longitude: 3.0619513,
+                address: '12 rue de rivoli - 59000'
+            }, 
+            image: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/5e/6d/eb/restaurant.jpg',
+            note: 4.8,
+            category: 'Italien',
+            createdAt: new Date(),
+        },
+        {
+            id: 3,
+            name: 'Salsa Posada',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim quidem autem dolore optio cupiditate dignissimos doloremque maiores repellat? Repudiandae quod accusamus consequuntur autem facere, nisi similique aperiam quia asperiores?',
+            location: {
+                latitude: 50.6321028,
+                longitude: 3.0619513,
+                address: '12 rue de rivoli - 59000'
+            }, 
+            image: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/5e/6d/eb/restaurant.jpg',
+            note: 3.2,
+            category: 'Mexicain',
+            createdAt: new Date(),
+        },
     ];
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(public alertCtrl: AlertController){}
 
+    ngOnInit() {
+    }
+
+    async showPreviewModal(){
+        const alert = await this.alertCtrl.create({
+            header: 'Bientôt disponible',
+            message: 'Cette fonctionnalité sera bientôt ajouté. Restez connecté !',
+            buttons: ['OK']
+        });
+
+        await alert.present();
+    }
 }
