ab33d8866a54cf9df6f933deb8f4c6a8454c4a37
Robin Delbaere <hello@rdelbaere.fr>
2021-04-13 16:51:35 +0200

    Correction : pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,11 +10,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {provide: LOCALE_ID, useValue: 'fr' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExpandDirective } from '../directives/expand.directive';
+import { CategoryIconPipe } from '../pipes/category-icon.pipe';
 
+const contents = [ExpandDirective,CategoryIconPipe];
 
 @NgModule({
-    declarations: [ExpandDirective],
+    declarations: contents,
     imports: [
         CommonModule
     ],
-    exports: [ExpandDirective]
+    exports: contents,
 })
 export class SharedModule { }
